dfa9dbc623661940c0f1f78ac00034484f57b103
0xkenta <kenta0558kiichi614@gmail.com>
2021-12-03 16:48:31 +0100

    transfer tests from util-contracts

---
+++ test/treasuryV2.ts
@@ -0,0 +1,136 @@
+import { expect, use } from 'chai'
+import { Signer } from 'ethers'
+import { solidity } from 'ethereum-waffle'
+import { ethers } from 'hardhat'
+import { toBigNumber } from './lib/number'
+import {
+	TreasuryV2,
+	MockAddressRegistry,
+	MockDev,
+	MockWithdrawV2,
+	MockProperty,
+} from '../typechain-types'
+
+use(solidity)
+
+describe('TreasuryV2', () => {
+	let deployer: Signer
+	let user: Signer
+	let property: Signer
+	let treasury: TreasuryV2
+	let mockDev: MockDev
+	let mockAddressRegistry: MockAddressRegistry
+	let mockWithdraw: MockWithdrawV2
+	let mockProperty: MockProperty
+
+	beforeEach(async () => {
+		;[deployer, user, property] = await ethers.getSigners()
+
+		const mockAddressRegistryFactory = await ethers.getContractFactory(
+			'MockAddressRegistry'
+		)
+		mockAddressRegistry = await mockAddressRegistryFactory.deploy()
+
+		const treasuryFactory = await ethers.getContractFactory('TreasuryV2')
+		treasury = await treasuryFactory.deploy()
+		await treasury.initialize(mockAddressRegistry.address)
+
+		const mockDevFactory = await ethers.getContractFactory('MockDev')
+		mockDev = await mockDevFactory.deploy()
+
+		const mockWithdrawFactory = await ethers.getContractFactory(
+			'MockWithdrawV2'
+		)
+		mockWithdraw = await mockWithdrawFactory.deploy(mockAddressRegistry.address)
+
+		const mockPropertyFactory = await ethers.getContractFactory('MockProperty')
+		mockProperty = await mockPropertyFactory.deploy()
+
+		await mockDev.transfer(mockWithdraw.address, '1000000000000000000000')
+		await mockAddressRegistry.setRegistry('Withdraw', mockWithdraw.address)
+		await mockAddressRegistry.setRegistry('Dev', mockDev.address)
+	})
+
+	describe('withdraw, ', () => {
+		it('Can hold a DEV token.', async () => {
+			const beforeBalance = await mockDev.balanceOf(treasury.address)
+			const propertyAddress = await property.getAddress()
+			await treasury.withdraw(propertyAddress, {
+				gasLimit: 2000000,
+			})
+			const afterBalance = await mockDev.balanceOf(treasury.address)
+			const diffBalance = afterBalance.sub(beforeBalance)
+			expect(diffBalance.toString()).to.be.equal('10000000000000000000')
+		})
+	})
+
+	describe('transferProperty, ', () => {
+		it('DEV token can be transferred to sender.', async () => {
+			const firstBalance = await mockProperty.balanceOf(treasury.address)
+			expect(firstBalance.toString()).to.be.equal('0')
+
+			await mockProperty.mint(treasury.address, '10000000000000000000')
+
+			const secondBalance = await mockProperty.balanceOf(treasury.address)
+			expect(secondBalance.toString()).to.be.equal('10000000000000000000')
+
+			const nextTreasuryFactory = await ethers.getContractFactory('TreasuryV2')
+			const nextTreasury: TreasuryV2 = await nextTreasuryFactory.deploy()
+			await nextTreasury.initialize(mockAddressRegistry.address)
+
+			const deployerAddress = await deployer.getAddress()
+
+			const beforeBalance = await mockProperty.balanceOf(deployerAddress)
+			expect(beforeBalance.toString()).to.be.equal('0')
+
+			await treasury.transferProperty(
+				mockProperty.address,
+				nextTreasury.address
+			)
+
+			const thirdBalance = await mockProperty.balanceOf(treasury.address)
+			expect(thirdBalance.toString()).to.be.equal('0')
+			const afterBalance = await mockProperty.balanceOf(nextTreasury.address)
+			expect(afterBalance.toString()).to.be.equal('10000000000000000000')
+		})
+
+		it('Can not be performed except by the owner.', async () => {
+			const nextTreasuryFactory = await ethers.getContractFactory('TreasuryV2')
+			const nextTreasury: TreasuryV2 = await nextTreasuryFactory.deploy()
+			await nextTreasury.initialize(mockAddressRegistry.address)
+
+			await expect(
+				treasury
+					.connect(user)
+					.transferProperty(mockProperty.address, nextTreasury.address)
+			).to.be.revertedWith('Ownable: caller is not the owner')
+		})
+	})
+
+	describe('transferDev, ', () => {
+		it('DEV token can be transferred to sender.', async () => {
+			const deployerAddress = await deployer.getAddress()
+			const propertyAddress = await property.getAddress()
+
+			await treasury.withdraw(propertyAddress, { gasLimit: 2000000 })
+			const beforeBalance = await mockDev
+				.balanceOf(deployerAddress)
+				.then(toBigNumber)
+
+			await treasury.transferDev()
+
+			const afterBalance = await mockDev
+				.balanceOf(deployerAddress)
+				.then(toBigNumber)
+
+			const diffBalance = afterBalance.sub(beforeBalance)
+			expect(diffBalance.toString()).to.be.equal('10000000000000000000')
+		})
+
+		it('Can not be performed except by the owner.', async () => {
+			await expect(treasury.connect(user).transferDev()).to.be.revertedWith(
+				'Ownable: caller is not the owner'
+			)
+		})
+	})
+})
